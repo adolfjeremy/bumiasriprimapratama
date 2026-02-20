3f659633c5a187df7419cb303d724da577f46cbc
adolfjeremy <adolf.jer@email.com>
2026-02-20 11:21:59 +0700

    initial website commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E73BE", // biru mockup
+        dark: "#0F172A",
+        light: "#fafafa",
+      },
+      container: {
+        center: true,
+        padding: "1.5rem",
+      },
+    },
+  },
+  plugins: [],
+};
