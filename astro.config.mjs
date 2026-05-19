08d344ffed2a13137c49d6f27f43dd5b2a3fd525
adolfjeremy <adolf.jer@email.com>
2026-05-19 21:24:56 +0700

    chore: remove ssr flag in astro config and update deployment workflow to include production install and PM2 restart

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,9 +14,6 @@ export default defineConfig({
   }),
   vite: {
     plugins: [/** @type {any} */ (tailwindcss())],
-    ssr: {
-      noExternal: true,
-    },
   },
 
   integrations: [sitemap()],
